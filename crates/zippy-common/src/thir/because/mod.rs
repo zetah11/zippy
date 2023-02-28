501de232bb9a1c442b7aaa49c819bc081600ee89
zetah11 <findusxi@protonmail.com>
2023-02-28 17:34:39 +0100

    :tada: zippy-q

--- crates/zippy-common/src/thir/because/mod.rs
+++
@@ -1,8 +0,0 @@
-use crate::message::Span;
-
-#[derive(Clone, Debug)]
-pub enum Because {
-    Annotation(Span),
-    Inferred(Span, Option<Box<Because>>),
-    Unified(Span),
-}
--- src/main.rs
+++ src/main.rs
@@ -1,74 +1,7 @@
-mod args;
-mod compile;
-mod console_driver;
-mod input;
-mod target;
+use simple_logger::SimpleLogger;
 
-use std::fs::remove_dir_all;
-use std::process::Command;
-
-use zippy_backend::c::emit;
-use zippy_frontend::{parse, ParseResult};
-use zippy_midend::elaborate;
-
-use anyhow::anyhow;
-use clap::Parser;
-use codespan_reporting::files::SimpleFiles;
-
-use self::args::Arguments;
-use self::compile::compile;
-use self::console_driver::ConsoleDriver;
-use self::input::read_file;
-use self::target::get_target;
-
-fn main() -> anyhow::Result<()> {
-    env_logger::init();
-
-    let mut args = Arguments::parse();
-
-    if let self::args::Command::Clean(opts) = args.command {
-        remove_dir_all(opts.artifacts)?;
-        return Ok(());
-    }
-
-    let target = get_target(&args);
-    let source = &args.options().path;
-
-    let src = read_file(source)?;
-
-    let mut files = SimpleFiles::new();
-    let file = files.add(source.to_string_lossy().into(), src.clone());
-
-    let mut driver = ConsoleDriver::new(&args, files);
-
-    if args.command.check() {
-        let ParseResult {
-            checked,
-            mut names,
-            entry,
-        } = parse(&mut driver, src, file);
-
-        let (mut types, context, decls) = elaborate(&mut driver, &mut names, checked, entry);
-
-        if args.command.build() {
-            let code = emit(&mut driver, &mut names, &mut types, &context, entry, decls);
-            let exec = compile(&args, &target, code)?;
-
-            if args.command.run() {
-                let status = Command::new(exec).args(args.options.drain(..)).status()?;
-
-                if !status.success() {
-                    let err = if let Some(code) = status.code() {
-                        anyhow!("program quit with code {code}")
-                    } else {
-                        anyhow!("program terminated by signal")
-                    };
-
-                    return Err(err);
-                }
-            }
-        }
-    }
-
-    Ok(())
+fn main() {
+    SimpleLogger::new().init().unwrap();
+    println!("hi!");
 }
+
